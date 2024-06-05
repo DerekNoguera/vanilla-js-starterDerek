6b2bd80f4c86240a3ce23eb94b478f3c62814b3c
DerekNoguera <dereknoguera9@gmail.com>
2024-06-05 15:47:35 -0600

    Hice el contador, hice que se eliminen los elementos con DELeeTE

--- src/js/api.js
+++ src/js/api.js
@@ -1,4 +1,6 @@
 
+// import { inputTarea } from "./index.js";
+
 const getTask = async () => {
     try {
         const response = await fetch('http://localhost:3000/api/task')
@@ -27,9 +29,29 @@ const postTaks = async () => { // funcion para recibir la apy
         console.error(error)
     }
 }
-const deleteTask = async (taskId) => {
+// const putTask = async (idc) => { // funcion para recibir la apy
+//     try {
+//         const response = await fetch('http://localhost:3000/api/task/' + idc, {
+//             method: 'PUT', // metodo post para que se haga un post de lo que añadi
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({
+//                 Task: inputTarea.value, // task va a ser igual a mi input que mi input es inputTarea.value,
+//                 asd: contador, 
+//                 status: 
+
+//             })
+//         });
+//         const data = await response.json();
+//         console.log(data);   
+//     } catch (error) {
+//         console.error("error")
+//     }
+// }
+const deleteTask = async (ID) => {
     try {
-        const deleteResponse = await fetch(`http://localhost:3000/api/task/${taskId}`, {
+        const deleteResponse = await fetch(`http://localhost:3000/api/task/${ID}`, {
             //recibe iD como parametro y lo concatena al API
             method: 'DELETE'
         });
